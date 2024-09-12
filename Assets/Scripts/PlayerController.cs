c17cdee874bca83a5ee87cad4664344d4861385b
Akash Kapoor <akashkapoor306gmail.com>
2024-09-11 21:01:21 -0400

    Created scripting animation and flipping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
+    float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerMovement();
+    }
+
+    void playerMovement()
+    {
+        Vector3 scale = transform.localScale;
+        speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed",Mathf.Abs(speed));
+
+        if(speed < 0f)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
     }
 }
