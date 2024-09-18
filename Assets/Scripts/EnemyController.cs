c268303d032f7f8c923f197bf65ad654e93062b9
Akash Kapoor <akashkapoor306gmail.com>
2024-09-17 20:06:48 -0400

    Added Enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private GameObject point_A;
+    [SerializeField] private GameObject point_B;
+    [SerializeField] private float speed = 1.0f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(speed, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "PointB")
+        {
+            speed = -1f;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if(collision.gameObject.name == "PointA")
+        {
+            speed = 1f;
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        }
+    }
+}
